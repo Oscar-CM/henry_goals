2b283eabdb607371d1374e3ee08e7ce12edd17ae
Oscar Maritim <oscarmaritim3@gmail.com>
2019-09-06 23:19:52 +0300

    adding goals

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'goal-project'`, () => {
+  it(`should have as title 'Goals'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('goal-project');
+    expect(app.title).toEqual('Goals');
   });
 
-  it('should render title in a h1 tag', () => {
+  it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to goal-project!');
+    expect(compiled.querySelector('.content span').textContent).toContain('Goals app is running!');
   });
 });
